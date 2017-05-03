93ca1feb636d504382e0201a82b10f5c707f253d
mathieu17 <mathieu.ba.17@gmail.com>
2017-05-03 14:22:21 +0200

    premier commit

---
+++ WindowsService1/ConnexionSql.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace WindowsService1
+{
+    class ConnexionSql
+    {
+       
+
+        private static MySqlConnection Cn = null;
+
+        //Constructor
+        private ConnexionSql(string serveur, string bdd, string user, string mdp)
+            {
+            string connectionString;
+            connectionString = "SERVER=" + serveur + ";" + "DATABASE=" + bdd + ";" + "UID=" + user + ";" + "PASSWORD=" + mdp;
+
+            Cn = new MySqlConnection(connectionString);
+        }
+
+        public static MySqlConnection getInstance(string serveur, string bdd, string user, string mdp)
+        {
+            if (null == Cn)
+            { // Premier appel
+              //  ConnexionSql.Initialize(unProvider, uneDataBase);
+
+                new ConnexionSql(serveur, bdd, user, mdp);
+
+            }
+            return Cn;
+        }
+
+        //open connection to database
+        public static void OpenConnection()
+        {
+            Cn.Open();
+        }
+
+        //Close connection
+        public static void CloseConnection()
+        {
+            Cn.Close();
+        }
+}
+}
+
+
+
